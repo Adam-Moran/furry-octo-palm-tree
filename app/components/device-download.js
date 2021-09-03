d7a1b119749ef3d4909d54bc049252ddcb88d9a9
Adam Moran <Adam.Moran@ivanti.com>
2021-09-03 10:55:05 +0100

    Loading device data from json. Device download component creation.

---
+++ app/components/device-download.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class DeviceDownloadComponent extends Component {
+  columnHeaders = ['Name', 'Device', 'Path', 'Status'];
+}
---
+++ tests/integration/components/device-download-test.js
@@ -0,0 +1,17 @@
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+module('Integration | Component | device-download', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<DeviceDownload />`);
+
+    assert.dom(this.element).hasText('Name Device Path Status');
+  });
+});
