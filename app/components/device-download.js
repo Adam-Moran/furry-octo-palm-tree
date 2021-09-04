6a018b6fd1476d496d7400761540b25a5107221d
Adam Moran <Adam.Moran@ivanti.com>
2021-09-04 10:41:11 +0100

    Adding additional sass. Handling device selection presentation.

--- app/components/device-download.js
+++ app/components/device-download.js
@@ -2,4 +2,14 @@ import Component from '@glimmer/component';
 
 export default class DeviceDownloadComponent extends Component {
   columnHeaders = ['Name', 'Device', 'Path', 'Status'];
+  devices = [];
+
+  constructor() {
+    super(...arguments);
+    this.devices = this.args.devices.map((device) => ({
+      ...device,
+      selected: false,
+      disabled: device.status != 'available',
+    }));
+  }
 }
