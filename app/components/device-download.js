027fb1c35e8ed46f47be538dedd8d1359317c18c
Adam Moran <Adam.Moran@ivanti.com>
2021-09-04 18:02:34 +0100

    Handling select all and single selection checkbox lolgic.

--- app/components/device-download.js
+++ app/components/device-download.js
@@ -1,8 +1,13 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class DeviceDownloadComponent extends Component {
   columnHeaders = ['Name', 'Device', 'Path', 'Status'];
-  devices = [];
+  @tracked devices = [];
+  @tracked selectedDevicesCount = 0;
+  @tracked isChecked = false;
+  @tracked isIndeterminate = false;
 
   constructor() {
     super(...arguments);
@@ -11,5 +16,67 @@ export default class DeviceDownloadComponent extends Component {
       selected: false,
       disabled: device.status != 'available',
     }));
+  }
+
+  get totalCountString() {
+    let prefix = '';
+    if (this.selectedDevicesCount == 0) {
+      prefix = 'None';
+    } else {
+      prefix = this.selectedDevicesCount;
+    }
+    return prefix + ' Selected';
+  }
+
+  @action
+  setChecked() {
+    const checked = !this.isChecked;
+
+    this.devices = this.devices.map((device) => {
+      if (device.disabled) {
+        return { ...device };
+      }
+
+      if (!device.selected && checked) {
+        this.selectedDevicesCount++;
+      }
+
+      if (this.deviceSelected && !checked) {
+        this.selectedDevicesCount--;
+      }
+
+      return {
+        ...device,
+        selected: checked,
+      };
+    });
+  }
+
+  @action
+  deviceSelected(device) {
+    if (!device.selected) {
+      this.selectedDevicesCount++;
+    } else {
+      this.selectedDevicesCount--;
+    }
+
+    const maxCheckedDevices = this.devices.filter(
+      (device) => !device.disabled
+    ).length;
+
+    if (this.selectedDevicesCount == maxCheckedDevices) {
+      this.isChecked = true;
+      this.isIndeterminate = false;
+
+      return;
+    }
+    if (this.selectedDevicesCount == 0) {
+      this.isChecked = false;
+      this.isIndeterminate = false;
+
+      return;
+    }
+
+    this.isIndeterminate = true;
   }
 }
